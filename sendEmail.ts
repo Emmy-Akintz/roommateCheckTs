fbd22cca895e4c1c3d0e4665a9bfd75ef3663423
Ayomide Akintan <emzyakints2005@gmail.com>
2024-02-21 16:04:57 +0100

    working on sendEmail

--- sendEmail.ts
+++ sendEmail.ts
@@ -1,49 +1,53 @@
 import nodemailer from 'nodemailer';
+import 'dotenv/config'
+import ElasticEmail from '@elasticemail/elasticemail-client'
+const client = ElasticEmail.ApiClient.instance
+const apiKey = client.authentications['apikey']
+apikey.apiKey = process.env.
 // D308
 // const email = ['igbekele.emmanuel@lmu.edu.ng', 'attentionofvc@lmu.edu.ng', 'sdo.dsa@lmu.edu.ng', 'dsa@lmu.edu.ng', 'prolusc@lmu.edu.ng']
 const email = ['emzyakints2005@gmail.com']
 
 const sendEmail = async (numberCount: Number) => {
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
+    let transporter = nodemailer.createTransport({
+        service: 'lmu.edu',
         auth: {
             user: process.env.MYEMAIL,
             pass: process.env.MYPASS
         }
     });
 
-    for (let j = 0; j < email.length; j++) {
-        var mailOptions = {
+    const sendPromises = email.map(async (recipient) => {
+        let mailOptions = {
             from: process.env.MYEMAIL,
-            to: email[j],
+            to: recipient,
             subject: 'WE NEED BETTER INTERNET IN DORCAS HALL',
             text: 'Good day sir, I trust this meets you well.',
             html: '<b>We are really limited and there are a couple of things that could be done in little time that instead takes a load of time and this is due to unreliable network. We\'d really appreciate if our internets is made much faster and reliable as soon as possible. THANK YOU SIR</b>'
         };
 
-        transporter.sendMail(mailOptions, function (error: any, info: any) {
-            if (error) {
-                console.log(error, numberCount, email[j])
-            } else {
-                console.log({ numberCount, message: 'Successfully sent to your mail' })
-            }
-        });
-    }
+        try {
+            const info = await transporter.sendMail(mailOptions);
+            console.log({ numberCount, message: 'Successfully sent to your mail' });
+            return info;
+        } catch (error) {
+            console.log(error, numberCount, recipient);
+            return error;
+        }
+    });
 
-}
+    return Promise.all(sendPromises);
+};
 
 const parentFunction = async () => {
-    for (let i = 1; i <= 2; i++) {
-        await sendEmail(i)
+    try {
+        for (let i = 1; i <= 2; i++) {
+            await sendEmail(i);
+        }
+    } catch (error) {
+        console.error('Error sending emails: ', error);
     }
-}
+};
+
 
 parentFunction()
-
-Error: getaddrinfo ENOTFOUND smtp.gmail.com
-    at GetAddrInfoReqWrap.onlookupall [as oncomplete] (node:dns:118:26) {
-  errno: -3008,
-  code: 'EDNS',
-  syscall: 'getaddrinfo',
-  hostname: 'smtp.gmail.com',
-  command: 'CONN'
